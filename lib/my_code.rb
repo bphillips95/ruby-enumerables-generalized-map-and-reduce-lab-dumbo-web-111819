b2cf8b4a71feba32e1106d2faa2938ddaf4644ae
Baruch Phillips <baruchphillips@gmail.com>
2019-11-12 20:27:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,11 +14,11 @@ def reduce(source_array,starting_value=nil)
     sum = starting_value
     i = 0 
   else 
-    sum = array[0]
+    sum = source_array[0]
     i  = 1 
   end
-   while i < array.length do 
-     sum = yield(sum,array[i])
+   while i < source_array.length do 
+     sum = yield(sum,source_array[i])
      i+=1 
    end
    sum 
